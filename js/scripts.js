d069c1e360f4281c8263d7b62925463778ef28e8
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-13 15:01:43 +0100

    - ADD style - SET event-listener - ADD condidtion for reset - ADD cycle for

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,51 @@
-// Test funzionamento
-console.log('Template');
+const buttonStart = document.querySelector('button');
+
+let gameSwitch = false;
+buttonStart.addEventListener('click', function(){
+    gameSwitch = true;
+
+    if (gameSwitch == true){ 
+        document.querySelector('.grill').innerHTML = '';
+    }
+
+
+    let cell;
+
+    for(let i = 0; i < 100; i++){
+
+        cell = document.createElement('div');
+        cell.innerHTML = (randomNumber(1, 100));
+        document.querySelector('.grill').append(cell);
+
+        cell.addEventListener('click', function(){
+            this.classList.toggle('active');
+            
+            console.log(this.innerHTML);
+        }
+        );
+
+    }
+
+    
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------- FUNCTIONS -------------------------------------------------
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  };
